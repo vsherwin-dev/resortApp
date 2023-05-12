160a608d8d4801cecfff16d737018038a5d92184
vsherwin-dev <100796048+vsherwin-dev@users.noreply.github.com>
2023-05-12 16:12:30 +0800

    add crud

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 
-const Product = require("./models/resort");
+const Resort = require("./models/resort");
 
 mongoose.set("strictQuery", false);
 
@@ -22,6 +22,43 @@ app.use(methodOverride("_method"));
 
 app.get('/', (req, res) => {
     res.send('This is the homepage!')
+});
+
+app.get('/resorts', async (req, res) => {
+    const resorts = await Resort.find({});
+    res.render('resorts/index', { resorts })
+});
+
+app.get('/resorts/new', (req, res) => {
+    res.render('resorts/new');
+});
+
+app.post('/resorts', async (req, res) => {
+    const resort = new Resort(req.body.resort);
+    await resort.save();
+    res.redirect(`/resorts/${resort._id}`)
+});
+
+app.get('/resorts/:id', async (req, res,) => {
+    const resort = await Resort.findById(req.params.id)
+    res.render('resorts/show', { resort });
+});
+
+app.get('/resorts/:id/edit', async (req, res) => {
+    const resort = await Resort.findById(req.params.id)
+    res.render('resorts/edit', { resort });
+});
+
+app.put('/resorts/:id', async (req, res) => {
+    const { id } = req.params;
+    const resort = await Resort.findByIdAndUpdate(id, { ...req.body.resort });
+    res.redirect(`/resorts/${resort._id}`)
+});
+
+app.delete('/resorts/:id', async (req, res) => {
+    const { id } = req.params;
+    await Resort.findByIdAndDelete(id);
+    res.redirect('/resorts');
 });
 
 app.listen(3000, () => {
